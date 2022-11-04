ccd82f7ab8b52206a5470e8a1bffe97fa7f1fe12
bruno-mo-brandao <107812749+bruno-mo-brandao@users.noreply.github.com>
2022-11-04 22:27:16 +0000

    Manual Scoreboard

--- MineSweeper Versus/Game/FishKaboom2/src/client/ClientView.java
+++ MineSweeper Versus/Game/FishKaboom2/src/client/ClientView.java
@@ -5,9 +5,12 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,7 +20,7 @@ import server.PlayerSocket;
 import util.Board;
 import util.Handler;
 
-public class ClientView extends JFrame{
+public class ClientView extends JFrame implements ActionListener{
 	
 	private static ClientView instance;
 		
@@ -35,15 +38,19 @@ public class ClientView extends JFrame{
 	
 	private Handler handler = new Handler();
 	//
-	private int points = 0;
+	public int points, pointsOther = 0;
 	
 	public Panel panel = new Panel();
-	public JLabel label;
+	public JLabel label, label2;
 	public String username;
+	public JButton button1 = new JButton("+100 points");
+	public JButton button2 = new JButton("+100 points");
+	public JButton button3 = new JButton("BOMB!");
+	public JButton button4 = new JButton("BOMB!");
 	
 	
-	private JLabel scoreboardLabel; //
-	public ClientView(String username) throws IOException {		
+	//private JLabel scoreboardLabel; //
+	public ClientView(String username) throws IOException  {		
 		
 		ClientView.setInstance(this);
 		setUsername (username);
@@ -63,17 +70,34 @@ public class ClientView extends JFrame{
 		this.setVisible(true);
 		
 		//*//
-		PlayerDetails playerDetails = new PlayerDetails();
-		int nPlayers = playerDetails.getNrPlayers();
+		//PlayerDetails playerDetails = new PlayerDetails();
+		//int nPlayers = playerDetails.getNrPlayers();
 		
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(4, 1));
-		panel.add(scoreboardLabel = new JLabel("SCOREBOARD"));
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 20, 20));
+		panel.setLayout(new GridLayout(0,3));
 		panel.setBackground(Color.GRAY);
 		this.add(panel, BorderLayout.SOUTH);
 		
-		JLabel label = new JLabel(username + " : " + points);
+		label = new JLabel(username + " : " + points);
+		label2 = new JLabel("Other Fisherman: " + pointsOther);
 		panel.add(label);
+		panel.add(button1);
+		button1.addActionListener(this);
+		panel.add(button3);
+		button3.addActionListener(this);
+		panel.add(label2);
+		panel.add(button2);
+		button2.addActionListener(this);
+		panel.add(button4);
+		button4.addActionListener(this);
+
+
+		
+		
+		//button1.addActionListener((ActionListener) this);
+		
+		//button3.addActionListener((ActionListener) this);
 		
 		//List<PlayerSocket> allPlayers = playerDetails.getPlayers();
 		
@@ -98,10 +122,46 @@ public class ClientView extends JFrame{
 	
 	
 	//
+	/*
 	public void actionPerformed(ActionEvent e) {
-		label.setText(username + " : " + points);
-		
+		Object obj = e.getSource();
+		if (obj == button1) {
+		 points+=100;
+		 label.setText(username + " : " + points);
+		} else if (obj == button2) {
+		 pointsOther+=100;
+		 label2.setText("Other Fisher: " + pointsOther);
+		}else if (obj == button3) {
+		 pointsOther-=500;
+		 label2.setText(username + " : " + points);
+		} else if (obj == button4) {
+		 pointsOther-=500;
+		 label2.setText("Other Fisher: " + pointsOther);
+
 	}	
-	
+	*/
+	public void actionPerformed(ActionEvent e) {
+
+		
+		Object obj = e.getSource();
+		if (obj == button1) {
+		 points+=100;
+		 label.setText(username + " : " + points);
+		} else if (obj == button2) {
+		 pointsOther+=100;
+		 label2.setText("Other Fisherman: " + pointsOther);
+		} else if (obj == button3) {
+			if (points <= 500) 
+			points= 0;
+			else points -=500;
+			 label.setText(username + " : " + points);
+		}else if (obj == button4) {
+			if (pointsOther <= 500) 
+			pointsOther= 0;
+			else pointsOther -=500;
+			 label2.setText("Other Fisherman: " + pointsOther);
+		}
+		}
 	
 }
+
